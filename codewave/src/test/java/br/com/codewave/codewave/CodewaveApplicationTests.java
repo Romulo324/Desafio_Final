648cc28efceb1a65033a4f1426315c9b945d0f7e
jucaprevost <jucaprevost2@gmail.com>
2023-05-15 23:00:04 -0300

    subindo desafio final

---
+++ codewave/src/test/java/br/com/codewave/codewave/CodewaveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codewave.codewave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
