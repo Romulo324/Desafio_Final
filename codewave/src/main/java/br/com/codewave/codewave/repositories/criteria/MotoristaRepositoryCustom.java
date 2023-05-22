9597d5020ae418fce1965895d275a173cdcf84c6
jucaprevost <jucaprevost2@gmail.com>
2023-05-22 20:09:56 -0300

    commit branch juan

---
+++ codewave/src/main/java/br/com/codewave/codewave/repositories/criteria/MotoristaRepositoryCustom.java
@@ -0,0 +1,6 @@
+package br.com.codewave.codewave.repositories.criteria;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface MotoristaRepositoryCustom {
+
+}
---
+++ codewave/src/main/java/br/com/codewave/codewave/services/DestinoService.java
@@ -0,0 +1,44 @@
+package br.com.codewave.codewave.services;
+
+import br.com.codewave.codewave.Models.Destino;
+import br.com.codewave.codewave.repositories.DestinoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DestinoService {
+    @Autowired
+    public DestinoRepository destinoRepository;
+
+    public void adicionar(Destino destinoQueSeraSalvo) {
+        destinoRepository.save(destinoQueSeraSalvo);
+    }
+
+    public List<Destino> listarTodos() {
+        return destinoRepository.findAll();
+    }
+
+    public Destino acharPorId(Integer id) {
+        Optional<Destino> optionalDestino = destinoRepository.findById(id);
+        if (optionalDestino.isEmpty()){
+            throw new RuntimeException("Destino Não Encontrado");
+        }
+        return optionalDestino.get();
+    }
+
+    public void atualizar(Integer id, Destino destino){
+        if (destinoRepository.existsById(id)){
+            destinoRepository.save(destino);
+        }
+    }
+
+    public void remove(Integer id) {
+        Destino pesquisarDestino = acharPorId(id);
+        if (destinoRepository.existsById(id)){
+            destinoRepository.deleteById(id);
+        }
+    }
+}
