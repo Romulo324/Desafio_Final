71cab3f082956b3d04ef7b01b28860387915753a
erickbraga <erickfelipedeandrade33@gmail.com>
2023-05-25 20:35:40 -0300

    security completo

--- codewave/src/main/java/br/com/codewave/codewave/dto/JwtRequestDto.java
+++ codewave/src/main/java/br/com/codewave/codewave/dto/JwtRequestDto.java
@@ -1,11 +1,9 @@
 package br.com.codewave.codewave.dto;
 
 import lombok.Data;
-import java.io.Serializable;
 
 @Data
-public class JwtRequestDto implements Serializable {
-    private static final long serialVersionUID = 1l;
+public class JwtRequestDto {
 
     private String username;
     private String password;
--- codewave/src/main/java/br/com/codewave/codewave/services/usuarioSecurity/JwtTokenService.java
+++ codewave/src/main/java/br/com/codewave/codewave/services/usuarioSecurity/JwtTokenService.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import java.io.Serializable;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -15,10 +14,8 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Service
-public class JwtTokenService implements Serializable{
-
-    private static final long serialVersionUID = -1652413099019301057L;
-    public static final long TEMPO_VALIDADE_TOKEN = 5 * 60 * 60; // 5 Horas
+public class JwtTokenService{
+    public static final long TEMPO_VALIDADE_TOKEN = 1000 * 5 * 60 * 60; // 5 Horas
 
     private String secret = "34753778214125442A462D4A614E645267556B58703273357638792F423F4528";
 
@@ -26,6 +23,7 @@ public class JwtTokenService implements Serializable{
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
+
     // Retorna expiration date do token jwt
     public Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
@@ -34,31 +32,39 @@ public class JwtTokenService implements Serializable{
         final Claims claims = getAllClaimFromToken(token);
         return claimsResolver.apply(claims);
     }
+
     // Para retornar qualquer informação do token nos iremos precisar da secret key
     private Claims getAllClaimFromToken(String token) {
-        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJwt(token).getBody();
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
     }
+
     // Check if the token haas expired
     private Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return  expiration.before(new Date());
     }
+
     // Gera token para user
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return doGenerateToken(claims, userDetails.getUsername());
     }
+
     // Cria o token e define o tempo de expiração pra ele
     private String doGenerateToken(Map<String, Object> claims, String subject) {
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+        return Jwts.builder().setClaims(claims).setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + TEMPO_VALIDADE_TOKEN + 1000))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
     }
+
     //valida o token
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = getUsernameFromToken(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
+
+
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
